a4c575483884e655dd210b83e3b511979fe38030
ChickenIT-TP <61769631+ChickenIT-TP@users.noreply.github.com>
2020-03-09 20:05:46 +0700

    bai2_tong_2_so
    Code Bài 2 tính tổng 2 số

---
+++ chamcode/bai2_tong2so/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace bai2_tong2so
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soA, soB;
+            Console.WriteLine("Nhap vao 2 so tu nhien");
+            soA = Convert.ToInt32(Console.Read());
+            soB = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Tong cua 2 so la:");
+            Console.WriteLine(Convert.ToString(soA + soB));
+        }
+    }
+}
